c2d4f057b2395fe5d44ec09edf026d22638f8268
King Kong <king-kong@gmail.com>
2023-01-30 09:17:25 -0800

    animal component

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -1,3 +1,11 @@
-export default function Animal() {
-  return <></>;
+export default function Animal({ animal }) {
+  return (
+    <>
+      <div className="animal-container">
+        <p className="animal-name">{animal.name}</p>
+        {animal.says}
+        <img src={`../../../public/animals/${animal.type}.svg`} />
+      </div>
+    </>
+  );
 }
